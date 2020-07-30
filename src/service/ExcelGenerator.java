4ee246933720ae7d4e62902aa59a45da52d937b6
unknown <vamsi3353@gmail.com>
2020-07-30 08:23:10 +0530

    vamsi

--- src/service/ExcelGenerator.java
+++ src/service/ExcelGenerator.java
@@ -1,5 +1,5 @@
 package service;
-/*
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -21,18 +21,41 @@ public class ExcelGenerator {
 	FileOutputStream out;
 	public HSSFWorkbook excelGenerate(Prograd prograd, List<Prograd> list) throws IOException {
 		try {
-
-
+			//object of HSSFWorkbook class
+			 HSSFWorkbook workbook=new HSSFWorkbook();
+			 //creation of sheet
+			 HSSFSheet sheet = workbook.createSheet("ProGrad Details");
+			 //creating a row head
+			 HSSFRow hRow=sheet.createRow(0);
+			 //create a columns
+			  hRow.createCell(0).setCellValue("Prograd ID");
+			  hRow.createCell(1).setCellValue("Name");
+			  hRow.createCell(2).setCellValue("Rating");
+			  hRow.createCell(3).setCellValue("Comment");
+			  hRow.createCell(4).setCellValue("Recommendation");
+			   
+			  //reading inputs from list and adding to each row 
+			  
+			  int i=0;
+			  for(Prograd prograd1 :list)
+			  {
+				  int j=i+1;
+				  HSSFRow hrow = sheet.createRow(j);
+				  hrow.createCell( 0).setCellValue(prograd1.getId());
+					hrow.createCell( 1).setCellValue(prograd1.getName());
+					hrow.createCell(2).setCellValue(prograd1.getRate());
+					hrow.createCell(3).setCellValue(prograd1.getComment());
+					hrow.createCell(4).setCellValue(prograd1.getRecommend());
+					i++;
+			  }
+				  
+			  
 			
-			// Type your code here
-			
-		
-			}
 			// Do not modify the lines given below
-			 out = new FileOutputStream(filename);
-			hwb.write(out);
+			 out = new FileOutputStream("V:\\EXCEL\\excellab.xlsx");
+			workbook.write(out);
 		
-			return hwb;
+			return workbook;
 			}
 		catch (Exception e) {
 				e.printStackTrace();
@@ -44,4 +67,3 @@ public class ExcelGenerator {
 		
 	}
 }
-*/
--- src/testing/TestPrograd.java
+++ src/testing/TestPrograd.java
@@ -1,5 +1,5 @@
 package testing;
-/* uncomment the below lines to test your code
+
 import org.junit.Test;
 
 import model.Prograd;
@@ -41,4 +41,4 @@ public class TestPrograd {
 		}
 	}
 }
-*/
+
